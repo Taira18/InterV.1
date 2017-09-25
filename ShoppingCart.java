619636f0f09538127b73184c450dd77dac4374b7
Taira18 <31222319+Taira18@users.noreply.github.com>
2017-09-25 07:26:50 -1000

    Add files via upload

---
+++ ShoppingCart.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class ShoppingCart implements Cart {
+    private double sum;
+    private double psum;
+    private double tax;
+    private double ship;
+    
+    ArrayList<SelectedItem> index = new ArrayList<SelectedItem>();
+
+    
+    public ShoppingCart(){
+    }
+    public void addItem(SelectedItem newItem) {
+    	index.add(newItem);
+        for (int i=0;i<index.size();i++){
+        	if(index.get(i).getItemNumber() == index.get(i).getItemNumber()){
+            	index.get(i).setQuantity(index.get(i).getQuantity());
+            }
+        }
+    }
+
+    public void deleteItem(int deleteItemNumber) {            
+    		for(int i = 0;i<index.size();i++)
+            	if (index.get(i).getItemNumber() == deleteItemNumber){
+                	index.remove(i);
+                }
+     }
+
+    public double getpsum() {
+        for(int i = 0; i<index.size();i++ ){
+        psum = psum + (index.get(i).getQuantity() * index.get(i).getUnitPrice());
+        }
+        return psum;
+    } 
+    public double getTax() {
+    	tax = psum * 0.045;
+        return tax;
+    }
+    public double getShipping() {
+        if(psum <= 10){
+           ship = 2.5;
+        }
+        else{
+            ship = psum*0.15;    
+        }
+		return ship;
+    }
+    public String toString(){
+    	sum = psum + tax + ship;
+        for(int i = 0;i<index.size();i++){
+        System.out.print("Item: " + index.get(i).getDescription() + "\n" + "Quantity: " + index.get(i).getQuantity() + "\n" + "Unit Price: $" + index.get(i).getUnitPrice() +"\n" + "Total Price: $" +index.get(i).getQuantity() * index.get(i).getUnitPrice() + "\n" + "\n");
+        }
+        System.out.print( "\n" + "Total: " + psum + "\n" + "Tax: " + tax +"\n" + "Shipping: " + ship +"\n" + "Grand total: " + sum);
+        return null;
+    }
+ 
+}
