8d671ef2fe2dc6d993ac43a9ec1e40f591daa9fa
Taira18 <31222319+Taira18@users.noreply.github.com>
2017-09-25 07:36:41 -1000

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -50,7 +50,7 @@ public class ShoppingCart implements Cart {
         for(int i = 0;i<index.size();i++){
         System.out.print("Item: " + index.get(i).getDescription() + "\n" + "Quantity: " + index.get(i).getQuantity() + "\n" + "Unit Price: $" + index.get(i).getUnitPrice() +"\n" + "Total Price: $" +index.get(i).getQuantity() * index.get(i).getUnitPrice() + "\n" + "\n");
         }
-        return "\n" + "Total: " + psum + "\n" + "Tax: " + tax +"\n" + "Shipping: " + ship +"\n" + "Grand total: " + sum;
+        return "Total: " + psum + "\n" + "Tax: " + tax +"\n" + "Shipping: " + ship +"\n" + "Grand total: " + sum;
     }
  
 }
